73982e419b01b21cc436ca17e2abd0566e185695
Ali Ganji <aliganji142879@gmail.com>
2025-02-09 16:11:40 +0330

    handle IP change errors by adding a wait time fallback"

--- request_with_proxy.py
+++ request_with_proxy.py
@@ -13,13 +13,22 @@ proxies = {'https': PROXY, 'http': PROXY}
 while True:
     try:
         response_one = requests.get(TARGET_URL, proxies=proxies, timeout=10)
+        print(response_one.text)
+
         response_second = requests.get(
             CHANGE_IP_URL, proxies=proxies, timeout=10)
-        print(response_one.text)
-        print(response_second.text)
+        change_data = response_second.json()
+
+        if "error_message" in change_data:
+            print(change_data['error_message'])
+            wait_time = change_data.get('left',15)
+            print(f"Waiting {wait_time} seconds before retrying...")
+            time.sleep(wait_time)
+        else:
+            print(change_data)
         with open("log_proxy.txt", "a") as a:
             a.write(
                 f"Request_One: {response_one.text}\nRequest_Second: {response_second.text}\n")
     except Exception as e:
         print(e)
-    time.sleep(3)
+    time.sleep(5)
